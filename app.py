5f4c0b7f31c639ec1fc9b9a180ac7a56d0d5fc06
lazaro <lachy9352@gmail.com>
2020-11-06 22:09:41 -0400

    creating connection to db

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.from_pyfile('config.cfg')
+
+db = SQLAlchemy(app)
+
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+if __name__ == '__main__':
+    app.run()
